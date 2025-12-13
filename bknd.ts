9b5e673faacd88c613a3a41fa61c90ba7b4d6052
cameronapak <cameronandrewpak@gmail.com>
2025-12-13 16:07:54 -0600

    simplify config into an object

--- bknd.ts
+++ bknd.ts
@@ -1,43 +1,41 @@
 import { createRuntimeApp } from 'bknd/adapter'
 import { em, entity, boolean, text } from 'bknd'
 import { sqlite } from 'bknd/adapter/sqlite'
-import type { App } from 'bknd'
+import type { App, BkndConfig } from 'bknd'
 import { Api } from 'bknd/client'
 import type { Context } from 'hono'
 
 const connection = sqlite({ url: ':memory:' })
+const config = {
+  connection,
+  config: {
+    data: em({
+      todos: entity('todos', {
+        title: text(),
+        done: boolean(),
+      }),
+    }).toJSON(),
+  },
+  onBuilt: async (app) => {
+    // This can only really run locally because it requires
+    // file access...
+    try {
+      await app.em.schema().sync({ force: true })
+    } catch (e) {
+      console.error(e)
+    }
+  },
+  options: {
+    mode: 'code',
+  },
+  adminOptions: {
+    adminBasepath: '/admin',
+    logoReturnPath: '/../',
+  },
+} as BkndConfig
 
 export async function getBkndApp(context: Context) {
-  const app = await createRuntimeApp(
-    {
-      connection,
-      config: {
-        data: em({
-          todos: entity('todos', {
-            title: text(),
-            done: boolean(),
-          }),
-        }).toJSON(),
-      },
-      onBuilt: async (app) => {
-        // This can only really run locally because it requires
-        // file access...
-        try {
-          await app.em.schema().sync({ force: true })
-        } catch (e) {
-          console.error(e)
-        }
-      },
-      options: {
-        mode: 'code',
-      },
-      adminOptions: {
-        adminBasepath: '/admin',
-        logoReturnPath: '/../',
-      },
-    },
-    context
-  )
+  const app = await createRuntimeApp(config, context)
   return app
 }
 
