56cd2d778923b5e748a9de1b3124ef191b0628d2
cameronapak <cameronandrewpak@gmail.com>
2025-12-20 08:25:34 -0600

    Add title to trash icon

--- src/index.tsx
+++ src/index.tsx
@@ -63,6 +63,7 @@ app.get('/', async (c) => {
                     stroke-linecap="round"
                     stroke-linejoin="round"
                   >
+                    <title>Trash</title>
                     <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
                   </svg>
                 </div>
